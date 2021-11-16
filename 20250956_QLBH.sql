64297681743322963b596d488af694b28d496e9f
NgTrungUIT <70690748+NguyenTrung1512@users.noreply.github.com>
2021-11-16 20:13:13 +0700

    Add files via upload

--- 20250956_QLBH.sql
+++ 20250956_QLBH.sql
@@ -445,6 +445,7 @@ SELECT TOP 3 MAKH, HOTEN
 FROM KHACHHANG 
 ORDER BY DOANHSO DESC
 
+
 -- Câu 28
 -- In ra danh sách các sản phẩm (MASP, TENSP) có giá bán bằng 1 trong 3 mức giá cao nhất
 SELECT SP.MASP, SP.TENSP
@@ -457,4 +458,155 @@ WHERE SP.GIA IN (
 
 
 -- Câu 29
--- 
+-- In ra danh sách các sản phẩm (MASP, TENSP) do “Thai Lan” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của tất cả các sản phẩm).
+SELECT SP.MASP, SP.TENSP
+FROM SANPHAM SP
+WHERE SP.NUOCSX = 'Thai Lan' AND SP.GIA IN ( 
+											SELECT TOP 3 GIA
+											FROM SANPHAM 
+											ORDER BY GIA DESC
+										   )
+
+
+-- Câu 30 
+-- In ra danh sách các sản phẩm (MASP, TENSP) do “Trung Quoc” sản xuất có giá bằng 1 trong 3 mức giá cao nhất (của sản phẩm do “Trung Quoc” sản xuất).
+SELECT SP.MASP, SP.TENSP
+FROM SANPHAM SP
+WHERE SP.NUOCSX = 'Trung Quoc' AND SP.GIA IN (
+												SELECT TOP 3 GIA 
+												FROM SANPHAM SP
+												WHERE SP.NUOCSX = 'Trung Quoc'
+												ORDER BY GIA DESC 
+											 )
+
+
+/* Câu 31
+-- In ra danh sách 3 khách hàng có doanh số cao nhất (sắp xếp theo kiểu xếp hạng)
+SELECT * 
+FROM KHACHHANG KH
+WHERE KH.MAKH = ( 
+	SELECT KH.MAKH = 
+		CASE 
+			WHEN KH.DOANHSO < MAX(
+				
+*/
+
+
+-- Câu 32
+-- Tính tổng số sản phẩm do “Trung Quoc” sản xuất.
+SELECT COUNT(*)
+FROM SANPHAM 
+WHERE NUOCSX = 'Trung Quoc'
+
+
+-- Câu 33
+-- Tính tổng số sản phẩm của từng nước sản xuất
+SELECT SP.NUOCSX, COUNT(*) AS 'Tong so sp'
+FROM SANPHAM SP
+GROUP BY SP.NUOCSX
+
+
+-- Câu 34
+-- Với từng nước sản xuất, tìm giá bán cao nhất, thấp nhất, trung bình của các sản phẩm
+SELECT SP.NUOCSX, MAX(SP.GIA) AS 'CAO NHAT',  MIN(SP.GIA) AS 'THAP NHAT', AVG(SP.GIA) AS 'TRUNG BINH '
+FROM SANPHAM SP
+GROUP BY SP.NUOCSX
+
+-- Câu 35
+-- Tính doanh thu bán hàng mỗi ngày
+SELECT HD.NGHD, SUM(HD.TRIGIA) as 'Doanh thu'
+FROM HOADON HD
+GROUP BY HD.NGHD
+
+
+-- Câu 36
+-- Tính tổng số lượng của từng sản phẩm bán ra trong tháng 10/2006
+SELECT CTHD.MASP, SUM(CTHD.SL) AS 'SO LUONG'
+FROM HOADON HD JOIN CTHD
+ON HD.SOHD = CTHD.SOHD
+WHERE MONTH(HD.NGHD) = 10 AND YEAR(HD.NGHD) = 2006
+GROUP BY CTHD.MASP 
+
+
+-- Câu 37
+-- Tính doanh thu bán hàng của từng tháng trong năm 2006
+SELECT MONTH(HD.NGHD), SUM(HD.TRIGIA) AS 'DOANH THU'
+FROM HOADON HD 
+GROUP BY MONTH(HD.NGHD) 
+
+
+-- Câu 38
+-- Tìm hóa đơn có mua ít nhất 4 sản phẩm khác nhau
+SELECT CTHD.SOHD
+FROM CTHD  
+GROUP BY CTHD.SOHD
+HAVING COUNT(*) >= 4
+
+
+-- Câu 39
+-- Tìm hóa đơn có mua 3 sản phẩm do “Viet Nam” sản xuất (3 sản phẩm khác nhau).
+SELECT SOHD
+FROM CTHD JOIN SANPHAM SP
+ON CTHD.MASP = SP.MASP
+WHERE SP.NUOCSX = 'Viet Nam'
+GROUP BY CTHD.SOHD
+HAVING COUNT(DISTINCT CTHD.MASP) >= 3	
+
+
+-- Câu 40
+--  Tìm khách hàng (MAKH, HOTEN) có số lần mua hàng nhiều nhất.
+SELECT KH.MAKH, KH.HOTEN
+FROM KHACHHANG KH JOIN HOADON HD
+ON KH.MAKH = HD.MAKH
+GROUP BY KH.MAKH, KH.HOTEN 
+HAVING COUNT(*) >= ALL ( 
+	SELECT COUNT(*)
+	FROM HOADON
+	GROUP BY MAKH
+					)
+
+
+-- Câu 41
+-- Tháng mấy trong năm 2006, doanh số bán hàng cao nhất ?
+SELECT MONTH(HD.NGHD) AS 'THANG'
+FROM HOADON HD
+WHERE YEAR(HD.NGHD) = 2006
+GROUP BY MONTH(HD.NGHD)
+HAVING SUM(HD.TRIGIA) >= ALL (
+		SELECT SUM(TRIGIA)
+		FROM HOADON
+		WHERE YEAR(NGHD) = 2006
+		GROUP BY MONTH(NGHD)
+	)
+
+
+-- Câu 42
+-- Tìm sản phẩm (MASP, TENSP) có tổng số lượng bán ra thấp nhất trong năm 2006.
+SELECT TOP 1 WITH TIES SP.MASP, SP.TENSP
+FROM SANPHAM SP, HOADON HD, CTHD
+WHERE SP.MASP = CTHD.MASP
+	  AND HD.SOHD = CTHD.SOHD
+	  AND YEAR(HD.NGHD) = 2006
+GROUP BY SP.MASP, SP.TENSP
+ORDER BY SUM(CTHD.SL) 
+
+
+/* Câu 43
+-- Mỗi nước sản xuất, tìm sản phẩm (MASP,TENSP) có giá bán cao nhất
+SELECT SP.NUOCSX, SP.MASP, SP.TENSP
+FROM SANPHAM SP
+GROUP BY SP.NUOCSX, SP.MASP, SP.TENSP
+HAVING MAX(SP.GIA) >= ALL (
+	SELECT MAX(SP.GIA)
+	FROM SANPHAM 
+	WHERE SP.GIA = GIA 
+	)
+*/
+
+
+-- Câu 44 
+-- Tìm nước sản xuất sản xuất ít nhất 3 sản phẩm có giá bán khác nhau
+SELECT NUOCSX
+FROM SANPHAM 
+GROUP BY NUOCSX
+HAVING COUNT(DISTINCT GIA) >= 3
